a4b3e7d9d75846c5d89467f7ef4f9b36be265011
Cuvillier--Couvreur Béryl <beryl.cuvillier@gmail.com>
2026-01-20 13:00:43 +0100

    re modif

--- jeux/test.py
+++ jeux/test.py
@@ -1,10 +1,10 @@
 from menu import *
+from option_joueur import *
+from regle import *
+from succes import *
 from jeu_calcul_mental import *
 from jeu_devinette_nombre import *
 from jeu_pendu import *
 from jeu_chifumi import *
-from option_joueur import *
-from regle import *
-from succes import *
 
 menu()
